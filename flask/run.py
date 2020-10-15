cb3619249c85d48d1fcd271c49b1f6d9baf4e176
Alissa Troiano <alissatroianonyc@gmail.com>
2020-10-15 18:37:27 +0000

    Import bootstrap theme intoand add stylesheet to base.html

--- flask/run.py
+++ flask/run.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -12,7 +11,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
-
 
 @app.route('/contact')
 def contact():
