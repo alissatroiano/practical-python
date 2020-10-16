8abd20859b46c2a9ef929d2c00e1a6edfc67cfbd
Alissa Troiano <alissatroianonyc@gmail.com>
2020-10-16 20:47:28 +0000

    Import and implement the flash function to provide user feedback after submitting form

--- flask/run.py
+++ flask/run.py
@@ -1,12 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/about')
 def about():
@@ -27,14 +29,19 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("contact.html", page_title="Contact")
 
 
 @app.route('/careers')
 def careers():
     return render_template("careers.html", page_title="Careers")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
